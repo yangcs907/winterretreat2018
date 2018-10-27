940136f252d76500cd12f86c74b9aa8e5d69f895
yangcs907 <>
2018-10-27 07:05:14 -0400

    added contact form

--- src/Contact.js
+++ src/Contact.js
@@ -4,14 +4,22 @@ import "./index.css";
 class Contact extends Component {
   render() {
     return (
-      <div className="contentDiv" style={{marginBottom:"100px"}}>
+      <div className="contentDiv" style={{marginBottom:"20px"}}>
         <p id="contentTitle">Contact Us</p>
         <p>Have a question? Please fill out the form below to contact us, or send an email directly to one of the addresses below</p>
         <p><span style={{fontWeight:"bold"}}>Chris Yang</span>&nbsp;&nbsp;<span style={{fontStyle:"italic",fontWeight:"normal",fontSize:"14px"}}>Supreme Leader</span> -&nbsp;<a href="">yangcs907@gmail.com</a></p>
         <p><span style={{fontWeight:"bold"}}>Chris Yang</span>&nbsp;&nbsp;<span style={{fontStyle:"italic",fontWeight:"normal",fontSize:"14px"}}>Supreme Leader</span> -&nbsp;<a href="">yangcs907@gmail.com</a></p>
         <p><span style={{fontWeight:"bold"}}>Chris Yang</span>&nbsp;&nbsp;<span style={{fontStyle:"italic",fontWeight:"normal",fontSize:"14px"}}>Supreme Leader</span> -&nbsp;<a href="">yangcs907@gmail.com</a></p>
         <br></br><br></br>
-        Form will go here
+        <div className="contactForm">
+          <form action="https://formspree.io/yangcs907@gmail.com"
+            method="POST">
+            <input type="text" name="Name" placeholder="Your Name (First and Last)" style={{fontFamily:"Avenir"}}/>
+            <input type="email" name="Email" placeholder="Your Email" style={{fontFamily:"Avenir"}} />
+            <textarea type="text" name="Message" placeholder="Message" style={{fontFamily:"Avenir"}} />
+            <input type="submit" value="Send" style={{fontFamily:"Avenir"}} id="sendButton" />
+          </form>
+        </div>
     </div>
     )
   };
